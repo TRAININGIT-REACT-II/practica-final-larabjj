af35add8859b433063e5a20e2b34fd58c9cba078
Pablo Lara Cerero <pablolc@pabloLC.technicolor.net>
2021-05-24 15:58:16 +0200

    views - first version

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -1,28 +1,40 @@
-import Me from "./Me";
-import Course from "./Course";
-import { useRouteMatch } from "react-router";
-import { Route, NavLink } from "react-router-dom";
+// import { useRouteMatch } from "react-router";
+// import { Route, NavLink } from "react-router-dom";
+// import Modal from "../components/Modal";
 
-const About = () => {
-  const match = useRouteMatch();
+const About = (props) => {
+  // const match = useRouteMatch();
 
-  return <section aria-labelledby="">
-
+  const { temp } = props;
 
 
-    <h4 id="about-title">Sobre el curso</h4>
-    <p>Pulsa para ver más detalle.</p>
+  // const [showModal, setShowModal] = useState(false);
+
+  // const openModal = () => setShowModal(true);
+  // const closeModal = () => setShowModal(false);
+
+  return <>
 
 
+    {/* <button className="btn btn-primary" onClick={openModal}>Show modal</button> */}
+
+    {/* <p>{temp}</p> */}
+
+    {/* <Modal show={showModal} onClose={closeModal}>
+      <h3>Title for modal</h3>
+      <p>Content for modal.</p>
+    </Modal> */}
+
+    {/* 
     <nav className="">
       <NavLink className="btn btn-outline-secondary" to={`${match.url}/me`}>Sobre mi</NavLink>
       <NavLink className="btn btn-outline-secondary" to={`${match.url}/course`}>Sobre este curso</NavLink>
     </nav>
 
     <Route path={`${match.url}/me`} component={Me} />
-    <Route path={`${match.url}/course`} component={Course} />
-    
-  </section>
+    <Route path={`${match.url}/course`} component={Course} /> */}
+
+  </>
 };
 
 export default About;
---
+++ client/src/views/Register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Register from '../components/Register/index'
+
+const RegisterPage = () => {
+  return (
+    <>
+      <h2>Register</h2>
+      <Register />
+    </>
+  )
+};
+
+export default RegisterPage;
